70baf08efe0ad03b5868872a1e27d01450b75ee9
CagyLocket <pawel.wrzesien@icloud.com>
2022-02-23 00:47:52 +0100

    settings.py tweaked

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('dfjndfijf8d77dfuifjj434uhd7f87yh3400cx0fh68sf')
+SECRET_KEY = ('dfjndfijf8d77dfuifjj434uhd7f87yh3400cx0fh68sf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'stormy-headland-77659.com']
+ALLOWED_HOSTS = [ 'stormy-headland-77659.com']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # During development only
